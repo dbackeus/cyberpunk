e1d731c1bfeddd3f2a2a24a363d1b7e00936c322
David Backeus <duztdruid@gmail.com>
2013-06-11 23:58:04 +0200

    Better boolean casting and some validation to character skill.

--- app/models/character_skill.rb
+++ app/models/character_skill.rb
@@ -12,14 +12,20 @@ class CharacterSkill
   field :type, type: String
   field :value, type: Integer, default: 0
 
+  validates :name, :ip_multiplier, presence: true
   validates :value, numericality: true
   validates :type, inclusion: { in: %w[career pickup chip] }
 
   def custom=(value)
-    write_attribute(:custom, value == "true")
+    write_attribute(:custom, boolean(value))
   end
 
   def special_ability=(value)
-    write_attribute(:special_ability, value == "true")
+    write_attribute(:special_ability, boolean(value))
+  end
+
+  private
+  def boolean(booleanish)
+    booleanish == true || booleanish == "true"
   end
 end

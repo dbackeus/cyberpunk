b208e61536100a5c376e80b020f7effd135df997
David Backeus <duztdruid@gmail.com>
2013-06-15 23:38:32 +0200

    Not using special ability id anymore.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ end
 YAML.load_file("db/roles.yml").each do |attributes|
   career_skill_names = attributes.delete('career_skills')
   career_skill_ids = Skill.where(:name.in => career_skill_names).collect(&:id)
-  Role.create!(attributes.merge(career_skill_ids: career_skill_ids, special_ability_id: special_ability.id))
+  Role.create!(attributes.merge(career_skill_ids: career_skill_ids))
   # Role.find_by(name: attributes['name']).update_attributes(career_skill_ids: career_skill_ids)
 end

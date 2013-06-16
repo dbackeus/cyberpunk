39468d956f707c1028f5cf6ea66b23fe31a0ae78
David Backeus <duztdruid@gmail.com>
2013-06-16 23:13:04 +0200

    Skill short name to keep things even.

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,10 +2,12 @@ class Skill
   include Mongoid::Document
 
   field :name, type: String
+  field :short, type: String
   field :stat, type: String
   field :description, type: String
   field :ip_multiplier, type: Integer, default: 1
   field :special_ability, type: Boolean, default: false
 
   validates :name, :ip_multiplier, presence: true
+  validates_length_of :short, maximum: 20, allow_nil: true
 end
